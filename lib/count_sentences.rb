6e3ba5a5bf39bfc561f7729d96a50057829a3a09
Ian <ianmcm.89@gmail.com>
2022-01-28 14:57:32 -0500

    commit tree f02b2424124325fd3365b936b2b6d565e100e7b0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]\s*/).filter {|string| string != ""}.count
   end
 end
+
+String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...").count_sentences
